30bf6c342cbbc5fe03c5ca0f5271ccac2760fbfe
okjong <ljo4954@gmail.com>
2022-11-02 15:20:54 +0900

    make button view

--- app/src/main/java/com/jeilpharm/githubtest/MainActivity.kt
+++ app/src/main/java/com/jeilpharm/githubtest/MainActivity.kt
@@ -2,13 +2,16 @@ package com.jeilpharm.githubtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val btn:Button= findViewById(R.id.btn)
+        btn.setOnClickListener { Toast.makeText(this, "click button", Toast.LENGTH_SHORT).show() }
 
     }
 }
