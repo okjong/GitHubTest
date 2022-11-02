94a2c70c10753304ec8609285dddc49f1bc850db
okjong <ljo4954@gmail.com>
2022-11-02 15:39:12 +0900

    add Texview

--- app/src/main/java/com/jeilpharm/githubtest/MainActivity.kt
+++ app/src/main/java/com/jeilpharm/githubtest/MainActivity.kt
@@ -3,6 +3,7 @@ package com.jeilpharm.githubtest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,9 @@ class MainActivity : AppCompatActivity() {
 
         val btn:Button= findViewById(R.id.btn)
         btn.setOnClickListener { Toast.makeText(this, "click button", Toast.LENGTH_SHORT).show() }
+
+        val tv:TextView=findViewById(R.id.tv)
+        tv.text="nice to meet u"
 
     }
 }
